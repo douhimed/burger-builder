92aa468ea28daa755f3ce80b332187658c42c461
douhimed <mr.douhi.med@gmail.com>
2019-09-16 22:21:47 +0100

    Adding action creators

---
+++ src/store/reducers/burgerBuilder.js
@@ -0,0 +1,48 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const initialState = {
+  ingredients: {
+    salad: 1,
+    bacon: 0,
+    cheese: 1,
+    tomato: 1,
+    meat: 1
+  },
+  price: 9.99
+};
+
+const INGREDIENTS_PRICES = {
+  salad: 1,
+  bacon: 1.5,
+  meat: 2.5,
+  cheese: 2,
+  tomato: 1.5
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        },
+        price: state.price + INGREDIENTS_PRICES[action.ingredientName]
+      };
+    case actionTypes.REMOVE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        },
+        price: state.price - INGREDIENTS_PRICES[action.ingredientName]
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
